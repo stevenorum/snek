9b4285d3e3f675cf80c3eaf931814d50e7464104
Steve Norum <stevenorum@gmail.com>
2019-02-28 17:06:41 -0500

    Bumping version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 MAJOR_VERSION='0'
 MINOR_VERSION='0'
-PATCH_VERSION='12'
+PATCH_VERSION='13'
 
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
